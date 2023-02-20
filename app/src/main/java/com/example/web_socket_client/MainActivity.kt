ee6c94cd2bb728d9c940700420c4ed7617391eba
Akshay_kumar <akshay.8459361606@gmail.com>
2023-02-20 19:54:40 +0530

    added tensorflow

--- app/src/main/java/com/example/web_socket_client/MainActivity.kt
+++ app/src/main/java/com/example/web_socket_client/MainActivity.kt
@@ -5,6 +5,8 @@ import android.graphics.BitmapFactory
 import android.graphics.Matrix
 import android.os.Bundle
 import android.util.Log
+import android.view.Surface
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.web_socket_client.databinding.ActivityMainBinding
@@ -13,13 +15,16 @@ import kotlinx.coroutines.launch
 import okhttp3.*
 import okio.ByteString
 import okio.ByteString.Companion.decodeHex
+import org.tensorflow.lite.task.vision.classifier.Classifications
+import kotlin.math.min
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ImageClassifierHelper.ClassifierListener {
     
     companion object {
         private const val NORMAL_CLOSURE_STATUS = 1000
     }
     
+    private lateinit var imageClassifierHelper: ImageClassifierHelper
     private lateinit var binding: ActivityMainBinding
     private lateinit var client: OkHttpClient
     
@@ -33,11 +38,13 @@ class MainActivity : AppCompatActivity() {
         binding.start.setOnClickListener {
             start()
         }
+
+
+        imageClassifierHelper = ImageClassifierHelper(context = this, imageClassifierListener = this)
     }
     
     private fun start() {
-//        val request: Request = Request.Builder().url("ws://" + "192.168.151.134" + ":86/").build()   // realme
-        val request: Request = Request.Builder().url("ws://" + "192.168.29.134" + ":86/").build()   // pixel
+        val request: Request = Request.Builder().url("ws://" + "192.168.234.134" + ":86/").build()
         
         val webSocketListener = object : WebSocketListener() {
             override fun onOpen(webSocket: WebSocket, response: Response) {
@@ -58,11 +65,14 @@ class MainActivity : AppCompatActivity() {
                     val bmp = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size) ?: return@launch
                     val viewWidth: Int = binding.imageView.width
                     val matrix = Matrix()
-                    matrix.postRotate(90f)
+//                    matrix.postRotate(90f)
                     val bmp_traspose = Bitmap.createBitmap(bmp, 0, 0, bmp.width, bmp.height, matrix, true)
                     val imagRatio = bmp_traspose.height.toFloat() / bmp_traspose.width.toFloat()
                     val dispViewH = (viewWidth * imagRatio).toInt()
-                    binding.imageView.setImageBitmap(Bitmap.createScaledBitmap(bmp_traspose, viewWidth, dispViewH, false))
+                    val bitmap = Bitmap.createScaledBitmap(bmp_traspose, viewWidth, dispViewH, false)
+                    binding.imageView.setImageBitmap(bitmap)
+                    
+                    imageClassifierHelper.classify(bitmap, Surface.ROTATION_0)
                 }
                 
             }
@@ -81,5 +91,26 @@ class MainActivity : AppCompatActivity() {
         val ws: WebSocket = client.newWebSocket(request, webSocketListener)
         client.dispatcher.executorService.shutdown()
     }
+    
+    override fun onError(error: String) {
+        lifecycleScope.launch(Dispatchers.Main) {
+            Toast.makeText(this@MainActivity, error, Toast.LENGTH_SHORT).show()
+        }
+    }
+    
+    override fun onResults(results: List<Classifications>?, inferenceTime: Long) {
+        updateResults(results)
+    }
+    
+    fun updateResults(listClassifications: List<Classifications>?) {
+        listClassifications?.let { it ->
+            if (it.isNotEmpty()) {
+                val sortedCategories = it[0].categories.sortedBy { it?.index }
+                binding.blindspot.text = sortedCategories.first().label
+                binding.confidence.text = String.format("%.2f", sortedCategories.first().score)
+            }
+        }
+    }
+    
     
 }
